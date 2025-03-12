ed19efcdf5a23208f7601cd336f1d45d0513c0d5
alessiomasala <alessio.masala@stud.unifi.it>
2025-03-12 16:35:57 +0100

    Aggiunto file .gitignore e rimosso file non necessari

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/alessiomasala/Uni/Primo Anno/laboratorio di programmazione/elaborato Alessio Masala
-# Build directory: /Users/alessiomasala/Uni/Primo Anno/laboratorio di programmazione/elaborato Alessio Masala/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[TestElaborato]=] "/Users/alessiomasala/Uni/Primo Anno/laboratorio di programmazione/elaborato Alessio Masala/cmake-build-debug/test_elaborato")
-set_tests_properties([=[TestElaborato]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/alessiomasala/Uni/Primo Anno/laboratorio di programmazione/elaborato Alessio Masala/CMakeLists.txt;31;add_test;/Users/alessiomasala/Uni/Primo Anno/laboratorio di programmazione/elaborato Alessio Masala/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
